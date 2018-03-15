7d36dc6b3ced9e6c53a6bfce4af0565742b06e21
somesh1993 <37415800+somesh1993@users.noreply.github.com>
2018-03-16 01:33:39 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApplication.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public int Mobile { get; set; }
+
+        public string Email { get; set; }
+
+        public int Id { get; set; }
+    }
+}
